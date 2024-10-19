ed046eff1facf6f777ff9c01fae3a984738e4417
Zachary Flohr <zachary.flohr@case.edu>
2024-10-19 16:27:44 -0400

    Uncapitalize non-readonly variable

--- lexicon/shared/notifications.sh
+++ lexicon/shared/notifications.sh
@@ -19,26 +19,26 @@
 ########################################################################
 print_message() {
     local -r MESSAGE="\n${3}"
-    local -i FOREGROUND_COLOR=7
+    local -i foreground_color=7
     case "${2}" in
         'red')
-            FOREGROUND_COLOR=1
+            foreground_color=1
         ;;
         'cyan')
-            FOREGROUND_COLOR=6
+            foreground_color=6
         ;;
         'gold')
-            FOREGROUND_COLOR=11
+            foreground_color=11
         ;;
         [[:digit:]]*)
-            FOREGROUND_COLOR=${2}
+            foreground_color=${2}
         ;;
     esac
-    tput sgr0 2> /dev/null                      # Turn off all attributes
-    (( ${1} )) && tput rev 2> /dev/null         # Turn on reverse video mode
-    tput bold 2> /dev/null                      # Turn on bold mode
-    tput setaf ${FOREGROUND_COLOR} 2> /dev/null   # Set foreground color
+    tput sgr0 2> /dev/null                          # Turn off all attributes
+    (( ${1} )) && tput rev 2> /dev/null             # Turn on reverse video mode
+    tput bold 2> /dev/null                          # Turn on bold mode
+    tput setaf ${foreground_color} 2> /dev/null     # Set foreground color
     (( ${1} )) && echo -e ${MESSAGE} >&2 || echo -e ${MESSAGE}
-    tput sgr0 2> /dev/null                      # Turn off all attributes
+    tput sgr0 2> /dev/null                          # Turn off all attributes
     return 0
 }
