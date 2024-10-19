1dae61ddaea8350cbda4901e9ebfb6a0db8eb630
Zachary Flohr <zachary.flohr@case.edu>
2024-10-19 00:23:24 -0400

    Move shared Bash functions to new directory

---
+++ lexicon/shared/notifications.sh
@@ -0,0 +1,24 @@
+# Functions for printing notifications to stdout and stderr.
+
+print_message() {
+    local -r MESSAGE="\n${3}"
+    local -i FOREGROUND_COLOR=7
+    case "${2}" in
+        'gold')
+            FOREGROUND_COLOR=11
+        ;;
+        'red')
+            FOREGROUND_COLOR=1
+        ;;
+        [[:digit:]]*)
+            FOREGROUND_COLOR=${2}
+        ;;
+    esac
+    tput sgr0 2> /dev/null                      # Turn off all attributes
+    (( ${1} )) && tput rev 2> /dev/null         # Turn on reverse video mode
+    tput bold 2> /dev/null                      # Turn on bold mode
+    tput setaf ${FOREGROUND_COLOR} 2> /dev/null   # Set foreground color
+    (( ${1} )) && echo -e ${MESSAGE} >&2 || echo -e ${MESSAGE}
+    tput sgr0 2> /dev/null                      # Turn off all attributes
+    return 0
+}
