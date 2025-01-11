7e6c8cce63ddf0a07ea626f72d6735facfa06c20
Zachary Flohr <zachary.flohr@case.edu>
2025-01-11 07:04:10 -0500

    Update block comment

--- shared/external_git_repos.sh
+++ shared/external_git_repos.sh
@@ -1,11 +1,8 @@
-# Author: Zachary Flohr
-#
 # Specify the paths of external Git repositories to be updated during
-# Git hooks as keys in the associative array GIT_REPO_TO_SOURCE_DIR_MAP.
-# The value of each key is the path of the source directory of the main
-# Git repository, relative to the repository's root, whence file diffs
-# are replicated during the post-commit hook to the external repository
-# whose path is the key.
+# Git hooks as keys in the associative array EXTERNAL_REPO_MAP.
+# The value of each key is the absolute path of the source Git repository
+# whence file diffs are replicated during the post-commit hook to the external
+# repository whose path is the key.
 
 declare -r GIT_DIR="${HOME}/github/"
 declare -Ar EXTERNAL_REPO_MAP=(
