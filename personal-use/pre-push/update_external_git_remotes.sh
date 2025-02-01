6c5b2e500027b030f53fe0a1f85136ca8e524838
Zachary Flohr <zachary.flohr@case.edu>
2025-02-01 10:40:05 -0500

    Improve code conciseness

--- personal-use/pre-push/update_external_git_remotes.sh
+++ personal-use/pre-push/update_external_git_remotes.sh
@@ -18,12 +18,12 @@ push_to_remote_repo() {
     print_message 0 "gold" "In ${1}"
     print_git_progress "fetch" "${REMOTE_URL}"
     git -C "${1}" fetch || terminate "git" "fetch" $?
-    print_git_progress "rev-list" "${REFNAMES[0]}"\
+    print_git_progress "rev-list" "${REFNAMES[0]}" \
         "${REFNAMES[1]}" "${SYM_DIFF[0]}"
-    [ "${1}" == "$(pwd)/" ] && print_git_progress "push" "${REMOTE_URL}" || {
-        ! (( ${SYM_DIFF[0]} )) || {
-            print_git_progress "push" "${REMOTE_URL}" &&
-                git -C "${1}" push || terminate "git" "push" $?
+    ! (( ${SYM_DIFF[0]} )) || {
+        print_git_progress "push" "${REMOTE_URL}"
+        [ "${1}" == "$(pwd)/" ] || {
+            git -C "${1}" push || terminate "git" "push" $?
         }
     }
 }
@@ -31,7 +31,7 @@ push_to_remote_repo() {
 main() {
     . $(dirname ${0})/../shared/external_git_repos.sh
     . $(dirname ${0})/../shared/notifications.sh
-    for git_repo in "${!GIT_REPO_TO_SOURCE_DIR_MAP[@]}" "$(pwd)/"; do
+    for git_repo in "${!EXTERNAL_REPO_MAP[@]}" "$(pwd)/"; do
         push_to_remote_repo "${git_repo}"
     done
 }
