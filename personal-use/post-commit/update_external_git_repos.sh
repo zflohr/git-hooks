9023d2282e435bdd96163ca6aa9ffc07a293cd3c
Zachary Flohr <zachary.flohr@case.edu>
2025-02-01 12:14:32 -0500

    Improve conciseness

--- personal-use/post-commit/update_external_git_repos.sh
+++ personal-use/post-commit/update_external_git_repos.sh
@@ -14,11 +14,8 @@ update_external_git_repo() {
     local -r COMMIT_MESSAGE=$(git log --pretty=format:"%B" -1 HEAD)
     local -ar ADDED_MODIFIED=(${added[*]} ${modified[*]})
     print_message 0 "gold" "In git repository ${1}"
-    (( ${#added[*]} )) && {
-        pushd ${1} > /dev/null
-        mkdir --parents $(dirname ${added[*]})
-        popd > /dev/null
-    }
+    (( ${#added[*]} )) &&
+        mkdir --parents $(dirname ${added[*]} | sed "s|^|${1}|")
     (( ${#ADDED_MODIFIED[*]} )) && {
         for file in ${ADDED_MODIFIED[*]}; do
             cp ${file} ${1}$(dirname ${file})
@@ -34,10 +31,6 @@ update_external_git_repo() {
 }
 
 get_diff_output() {
-    [ -d ${1} ] && [ -d ${EXTERNAL_REPO_MAP["${1}"]} ] &&
-        [ $(git -C "${EXTERNAL_REPO_MAP["${1}"]}" rev-parse --show-toplevel) ==\
-            $(git rev-parse --show-toplevel) ] || return
-    pushd ${EXTERNAL_REPO_MAP["${1}"]} > /dev/null
     local -a diff_statuses=($(git show --no-renames --name-status \
         --pretty=format:"%N" HEAD))
     local -a added=() deleted=() modified=()
@@ -62,14 +55,16 @@ get_diff_output() {
         [[ ${LAST_STASH_ENTRY_BEFORE} == ${LAST_STASH_ENTRY_AFTER} ]] ||
             git stash pop --quiet --index
     }
-    popd > /dev/null
 }
 
 main() {
     . $(dirname ${0})/../shared/external_git_repos.sh
     . $(dirname ${0})/../shared/notifications.sh
     for git_repo in "${!EXTERNAL_REPO_MAP[@]}"; do
-        get_diff_output "${git_repo}"
+        [ -d ${git_repo} ] && [ -d ${EXTERNAL_REPO_MAP["${git_repo}"]} ] &&
+            [ $(git -C "${EXTERNAL_REPO_MAP["${git_repo}"]}" rev-parse \
+                --show-toplevel) == $(git rev-parse --show-toplevel) ] &&
+            get_diff_output "${git_repo}"
     done
     print_message 0 "gold" "In $(pwd)/"
     print_git_progress "commit"
